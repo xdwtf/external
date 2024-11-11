00ecf080a75ba2dc67606775973b1adc525e0fba
Nikhil <71930916+dishapatel010@users.noreply.github.com>
2024-11-11 11:33:29 +0530

    Update amrea.py

--- amrea.py
+++ amrea.py
@@ -1,43 +1,61 @@
 import re
-from ub_core import BOT, bot, Message
 from pyrogram import filters
+from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup
+from ub_core import BOT, bot, Message
 
-# New tag to replace the old tag
+# New tag and username to replace the old tag and username
 new_tag = "isthisdeal-21"
+new_username = "@thisdeal"
 
 # Regular expression to find the tag parameter in the URL
-pattern = r"(tag=)[^&]*"
+pattern_tag = r"(tag=)[^&]*"
+pattern_username = r"@SmartDealsOfindia"
 
-# Target chat IDs as a list
+# Target chat ID
 TARGET_CHAT_ID = [-1001267968308, -1001628270160]
 THIS_DEAL_ID = -1002108741045
 
-@bot.on_message(filters.chat(TARGET_CHAT_ID), group=4)  # Listen to messages in group with ID 4
+@bot.on_message(filters.chat(TARGET_CHAT_ID), group=4)  # Listening to messages from the specified channel
 async def url_replacement_handler(bot: BOT, message: Message):
-    # Check if the message has a photo and a caption
+    text_to_check = None
+
+    # If the message has a photo and a caption, check caption
     if message.photo and message.caption:
         text_to_check = message.caption
-    # Check if the message is text
+    # If the message is a text message
     elif message.text:
         text_to_check = message.text
-    else:
-        text_to_check = None
-
+    
     if text_to_check:
-        # Replace the old tag with the new tag in all URLs within the text
-        modified_message = re.sub(pattern, f"tag={new_tag}", text_to_check)
-
-        # Send the modified message as text or photo with new caption to the target chat
-        if message.photo and message.caption:
+        # Replace the old tag with the new tag in the caption
+        modified_caption = re.sub(pattern_tag, f"tag={new_tag}", text_to_check)
+        # Replace the old username with the new username in the caption
+        modified_caption = re.sub(pattern_username, new_username, modified_caption)
+        
+        # Modify the button URLs
+        if message.reply_markup:
+            for row in message.reply_markup.inline_keyboard:
+                for button in row:
+                    if button.url:
+                        # Replace the old tag in the button URL
+                        button.url = re.sub(pattern_tag, f"tag={new_tag}", button.url)
+                        # Replace the old username with the new username in the button text
+                        if button.text:
+                            button.text = button.text.replace("@SmartDealsOfindia", "@thisdeal")
+        
+        # Send the modified message to the target chat
+        if message.photo:
             await bot.send_photo(
                 chat_id=THIS_DEAL_ID,
-                photo=message.photo.file_id,  # Use the existing photo file ID
-                caption=modified_message,  # Send modified caption
+                photo=message.photo.file_id,
+                caption=modified_caption,  # Send modified caption
+                reply_markup=message.reply_markup,  # Send modified inline keyboard
                 disable_web_page_preview=True
             )
         else:
             await bot.send_message(
                 chat_id=THIS_DEAL_ID,
-                text=modified_message,
+                text=modified_caption,
+                reply_markup=message.reply_markup,  # Send modified inline keyboard
                 disable_web_page_preview=True
             )
